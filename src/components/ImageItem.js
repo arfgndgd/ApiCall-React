fae7f8c1488c4eda0da18f3e9900b0f7ce367f8a
arfgndgd <gundogduarif4@gmail.com>
2023-08-02 11:45:56 +0300

    css

--- src/components/ImageItem.js
+++ src/components/ImageItem.js
@@ -1,6 +1,10 @@
 function ImageItem({image}) {
     console.log(image)
-    return ( <div></div> );
+    return ( 
+        <div>
+            <img className="imageListImg" src={image.urls.small} alt={image.alt_description}/>
+        </div> 
+    );
 }
 
 export default ImageItem;
--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -2,7 +2,7 @@ import ImageItem from "./ImageItem";
 
 function ImageList({imagesPlaceholder}) {
     return ( 
-    <div>
+    <div className="imageList">
         {imagesPlaceholder.map((image, index) => {
            return <ImageItem key={index} image={image}/>
         })}
